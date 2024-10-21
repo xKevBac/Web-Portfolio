8d27d597342b2627976ac191a589bf7c3ec2816e
xKevBac <kbach829@gmail.com>
2024-10-21 13:08:42 -0700

    update

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,18 +1,10 @@
 import './App.css';
-import React, { useState } from 'react';
-import Navbar from './Components/Navbar/Navbar';
 import Intro from './Components/Intro/Intro';
 
 function App() {
-  const [activePage, setActivePage] = useState('home');  // Set default page to 'home'
-
   return (
     <div>
-      <Navbar setActivePage={setActivePage} />
-      {activePage === 'home' && <Intro />}
-      {activePage === 'about' && <About />}
-      {activePage === 'projects' && <Projects />}
-      {activePage === 'contact' && <Contact />}
+      <Intro /> 
     </div>
   );
 }
